efbf301caee703d7b7eb2a0e63b148dfe55ab5b6
JohannesHa98 <Johannesh1998@hotmail.com>
2024-08-29 12:22:01 +0200

    Add Bootstrap import

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 export default function Home() {
   return (
