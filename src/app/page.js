730c6c7fdc107c9ff5eebc2e9f911215f91c6d67
JohannesHa98 <Johannesh1998@hotmail.com>
2024-08-29 12:42:13 +0200

    Fix Bootstrap import

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,24 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+'use client';
+
+import { useEffect } from 'react';
+import Image from 'next/image';
+import styles from './page.module.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 
 export default function Home() {
+  useEffect(() => {
+    import('bootstrap/dist/js/bootstrap.bundle.min.js')
+      .then((module) => {
+      })
+      .catch((err) => {
+        console.error('Failed to load Bootstrap JavaScript', err);
+      });
+  }, []);
+
   return (
     <main>
       <div>
-        
+
       </div>
     </main>
   );
