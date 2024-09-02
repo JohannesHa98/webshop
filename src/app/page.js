8344ec48e127ee1a42c9385b171ee776d25017ef
JohannesHa98 <Johannesh1998@hotmail.com>
2024-09-02 08:37:35 +0200

    Fix png issue

--- src/app/page.js
+++ src/app/page.js
@@ -186,7 +186,7 @@ export default function Home() {
                 </div>
                 <div className="image-container">
                   <Image
-                    src="/images/shoepng.png"
+                    src="/images/shoe.png"
                     alt="Shoe"
                     className="rotated-image"
                     width={500}
@@ -214,7 +214,7 @@ export default function Home() {
                 </div>
                 <div className="image-container">
                   <Image
-                    src="/images/shoepng.png"
+                    src="/images/shoe.png"
                     alt="Shoe"
                     className="rotated-image"
                     width={500}
